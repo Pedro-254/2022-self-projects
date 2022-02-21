dc47241c88a70ecd42f4dc688256c1b8ef4354b7
Pedro <Pedrolucasadorno254@gmail.com>
2022-02-21 14:12:53 -0300

    21/02/2022

--- jogo 2048.py
+++ jogo 2048.py
@@ -44,6 +44,7 @@ class Game:
             
     def Moviment(self):
         move = input("Movimento:")
+        hv_move = 0
         # Right
         if move == "d":
             # Iterar pelos elementos
@@ -58,11 +59,13 @@ class Game:
                                 self.matriz[line][colum] = 0
                                 self.matriz[line][colum2] = element
                                 colum = colum2
+                                hv_move = 1
                             elif space == element:
                                 self.matriz[line][colum] = 0
                                 self.matriz[line][colum2] = element + space
+                                hv_move = 1
                                 break
-                            elif space > element:
+                            else:
                                 break
             pass            
         # Left
@@ -78,23 +81,64 @@ class Game:
                                 self.matriz[line][colum] = 0
                                 self.matriz[line][colum2] = element
                                 colum = colum2
+                                hv_move = 1
                                 continue
                             elif space == element:
                                 self.matriz[line][colum2] = element + space
                                 self.matriz[line][colum] = 0
+                                hv_move = 1
                                 break
-                            elif space > element:
+                            else:
                                 break
             pass
         # Up
         elif move == "w":
+            for colum in range(4):
+                for line in range(4):
+                    element = self.matriz[line][colum]
+                    # Exlusao do elemento zero e elementos da coluna direita
+                    if element != 0 and line != 0:
+                        for line2 in range(line-1,-1,-1):
+                            space = self.matriz[line2][colum]
+                            if space == 0:
+                                self.matriz[line][colum] = 0
+                                self.matriz[line2][colum] = element
+                                line = line2
+                                hv_move = 1
+                            elif space == element:
+                                self.matriz[line2][colum] = element + space
+                                self.matriz[line][colum] = 0
+                                hv_move = 1
+                                break
+                            else:
+                                break
             pass
         # Down
         elif move == "s":
+            for colum in range(4):
+                for line in range(3,-1,-1):
+                    element = self.matriz[line][colum]
+                    # Exlusao do elemento zero e elementos da coluna direita
+                    if element != 0 and line != 3:
+                        for line2 in range(line+1,4):
+                            space = self.matriz[line2][colum]
+                            if space == 0:
+                                self.matriz[line][colum] = 0
+                                self.matriz[line2][colum] = element
+                                line = line2
+                                hv_move = 1
+                            elif space == element:
+                                self.matriz[line2][colum] = element + space
+                                self.matriz[line][colum] = 0
+                                hv_move = 1
+                                break
+                            else:
+                                break
             pass
         else:
             print("Invalid input!")
             self.Moviment()
+        if hv_move == 1:    self.new_number()
 
     # def end(self):
     #   for line in range(4):
@@ -108,15 +152,17 @@ class Game:
     #     # sys.exit("U lose!")
     #   else:
     #     pass
+    
+    # Para finalizar o jogo eu tenho que pegar todos os elementos e ver se seus adjacentes são 
         
     def start(self): 
         print(Menu())
         if input("Start the game? (y/n)") == "y":
+            self.new_number()
             self.state = 1
         else:
             return print("Jogo finalizado!")
         while (self.state == 1):
-            self.new_number()
             self.print_matriz()
             self.Moviment()
 
